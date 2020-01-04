76c80e0b99c76b15c2e6faebeb37ba51ec5d401d
RadmirKashapov <radmir.kashapov.150700@gmail.com>
2020-01-04 13:11:33 +0300

    Updated solution. Created presentation, business, rata access layers

---
+++ SmartHouse/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SmartHouse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
